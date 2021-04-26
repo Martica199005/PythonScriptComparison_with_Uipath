ac1d96c02e8691ce5cc67b0c3ce63af7d403409b
Martica <58595345+Martica199005@users.noreply.github.com>
2021-04-26 11:57:47 +0200

    Add files via upload

---
+++ StringComparison.py
@@ -0,0 +1,29 @@
+import sys
+import os
+#path "C:\ProgramData\Anaconda3\Lib\site-packages\fuzzywuzzy"
+#pip install fuzzywuzzy
+#pip install fuzzywuzzy[speedup]
+#pip install python-Levenshtein
+
+import importlib
+
+pyfilepath='C:\\ProgramData\\Anaconda3\\Lib\\site-packages\\fuzzywuzzy'
+dirname, basename = os.path.split(pyfilepath) # pyfilepath: /my/path/mymodule.py
+sys.path.append(dirname) # only directories should be added to PYTHONPATH
+module_name = os.path.splitext(basename)[0] # /my/path/mymodule.py --> mymodule
+module = importlib.import_module(module_name) # name space of defined module (otherwise we would literally look for "module_name")
+
+print(module)
+
+
+import fuzzywuzzy
+
+from fuzzywuzzy import fuzz
+
+def string_comparison(data,data1):
+    try:
+        return fuzz.ratio(data,data1)
+    except:
+        return "Error"
+
+
